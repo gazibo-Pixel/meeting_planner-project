948ace90590dff9ff19548c48f235102b18938c1
Taha <Mohamed.Taha@charter.com>
2021-08-02 11:05:01 -0500

    goodrom

--- meetings/views.py
+++ meetings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Room
 from django.forms import modelform_factory
 def details (request, id):
@@ -11,5 +11,11 @@ def room_list(request):
 MeetingForm = modelform_factory(Meeting, exclude=[])
 
 def new(request):
-    form = MeetingForm()
+    if request.method == "POST":
+        form = MeetingForm (request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ("home")
+    else:
+        form = MeetingForm()
     return render(request, "meetings/new.html", {'form':form})
